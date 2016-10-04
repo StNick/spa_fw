13d2dfd48a564fc5f78e791967674e922b7606e2
Nick Soper <nicksoper@gmail.com>
2016-10-04 20:59:28 +0200

    One more module to go

--- ext-modules/myDashboard/myDashboardDirective.js
+++ ext-modules/myDashboard/myDashboardDirective.js
@@ -1,7 +1,13 @@
 "use strict";
 
-angular.module("myDashboard").directive("myDashboard", function(){
+angular.module('myDashboard').directive('myDashboard', function(){
     return {
-        templateUrl: "ext-modules/myDashboard/myDashboardTemplate.html"
+        templateUrl: 'ext-modules/myDashboard/myDashboardTemplate.html',
+        link: function(scope, element, attrs){
+            scope.addNewWidget = function(widget){
+                var newWidget = angular.copy(widget.settings);
+                scope.widgets.push(newWidget);
+            }
+        }
     };
 });
--- ext-modules/myDashboard/myDashboardModule.js
+++ ext-modules/myDashboard/myDashboardModule.js
@@ -1,3 +1,3 @@
 "use strict";
 
-angular.module("myDashboard", ["gridster"]);
+angular.module("myDashboard", ["gridster", "ui.bootstrap"]);
